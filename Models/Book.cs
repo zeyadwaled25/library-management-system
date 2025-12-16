a2ab11773cdf7a2c8fd1f547d5472fbcdf4f0ea2
Abdulhamid-Iskander <abdulhamidiskander@gmail.com>
2025-12-17 01:36:03 +0200

    Complete BooksForm Logic and Search

---
+++ Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public int CategoryID { get; set; } 
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class CategoryItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
